ff82f49d82d1e192e976672821f295c4d298aaae
nielsonGonzaga <nielsondiasgonzaga123@gmail.com>
2018-08-28 19:54:33 -0300

    Projeto Spring orn

---
+++ src/Fvs/edu/dominio/Aluno.java
@@ -0,0 +1,144 @@
+package Fvs.edu.dominio;
+
+import java.io.Serializable;
+
+public class Aluno implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+	private Integer id;
+	private String nome;
+	private String cor;
+	private String rg;
+	private String matricula;
+	
+	
+	
+	public Aluno(Integer id, String nome, String cor, String rg, String matricula) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.cor = cor;
+		this.rg = rg;
+		this.matricula = matricula;
+	}
+	
+	
+	
+	@Override
+	public String toString() {
+		return "Aluno [id=" + id + ", nome=" + nome + ", cor=" + cor + ", rg=" + rg + ", matricula=" + matricula + "]";
+	}
+
+	
+
+	public Integer getId() {
+		return id;
+	}
+
+
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+
+
+	public String getNome() {
+		return nome;
+	}
+
+
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+
+
+	public String getCor() {
+		return cor;
+	}
+
+
+
+	public void setCor(String cor) {
+		this.cor = cor;
+	}
+
+
+
+	public String getRg() {
+		return rg;
+	}
+
+
+
+	public void setRg(String rg) {
+		this.rg = rg;
+	}
+
+
+
+	public String getMatricula() {
+		return matricula;
+	}
+
+
+
+	public void setMatricula(String matricula) {
+		this.matricula = matricula;
+	}
+
+
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((cor == null) ? 0 : cor.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((matricula == null) ? 0 : matricula.hashCode());
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		result = prime * result + ((rg == null) ? 0 : rg.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Aluno other = (Aluno) obj;
+		if (cor == null) {
+			if (other.cor != null)
+				return false;
+		} else if (!cor.equals(other.cor))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (matricula == null) {
+			if (other.matricula != null)
+				return false;
+		} else if (!matricula.equals(other.matricula))
+			return false;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
+		if (rg == null) {
+			if (other.rg != null)
+				return false;
+		} else if (!rg.equals(other.rg))
+			return false;
+		return true;
+	}
+	
+	
+	
+}
