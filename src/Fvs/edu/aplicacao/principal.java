e4aed152bce4c626d478613ea7ed318033607077
nielsonGonzaga <nielsondiasgonzaga123@gmail.com>
2018-09-10 18:59:47 -0300

    aula jpa2

--- src/Fvs/edu/aplicacao/principal.java
+++ src/Fvs/edu/aplicacao/principal.java
@@ -1,16 +1,25 @@
-package Fvs.edu.aplicacao;
+package fvs.edu.aplicacao;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
-public class principal {
+import fvs.edu.dominio.Aluno;
+
+public class Principal {
 
 	public static void main(String[] args) {
-		EntityManagerFactory emf=Persistence.createEntityManagerFactory("topicos-fvs");
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("topicos-fvs");
+		EntityManager em = emf.createEntityManager();
 		
-		EntityManager em = emf.createEntityManager(); 
-
+		Aluno aluno = em.find(Aluno.class, 1);
+		System.out.println(aluno);
+		
+		/*em.getTransaction().begin();
+		Aluno aluno1 = new Aluno(null, "Brenno", "123", "123", 9673);
+		em.persist(aluno1);
+		em.getTransaction().commit();
+		System.out.println("Salvo com sucesso!");*/
 	}
 
 }
--- src/Fvs/edu/dominio/Aluno.java
+++ src/Fvs/edu/dominio/Aluno.java
@@ -1,101 +1,87 @@
-package Fvs.edu.dominio;
+package fvs.edu.dominio;
 
 import java.io.Serializable;
 
-public class Aluno implements Serializable{
-	
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Aluno implements Serializable {
+
 	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue
 	private Integer id;
 	private String nome;
-	private String cor;
+	private String cpg;
 	private String rg;
-	private String matricula;
+	private Integer matricula;
 	
+	public Aluno() {
+		
+	}
 	
-	
-	public Aluno(Integer id, String nome, String cor, String rg, String matricula) {
+	public Aluno(Integer id, String nome, String cpg, String rg, Integer matricula) {
 		super();
 		this.id = id;
 		this.nome = nome;
-		this.cor = cor;
+		this.cpg = cpg;
 		this.rg = rg;
 		this.matricula = matricula;
 	}
 	
-	
-	
 	@Override
 	public String toString() {
-		return "Aluno [id=" + id + ", nome=" + nome + ", cor=" + cor + ", rg=" + rg + ", matricula=" + matricula + "]";
+		return "Aluno [id=" + id + ", nome=" + nome + ", cpg=" + cpg + ", rg=" + rg + ", matricula=" + matricula + "]";
 	}
-
-	
 
 	public Integer getId() {
 		return id;
 	}
 
-
-
 	public void setId(Integer id) {
 		this.id = id;
 	}
-
-
 
 	public String getNome() {
 		return nome;
 	}
 
-
-
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
 
-
-
-	public String getCor() {
-		return cor;
+	public String getCpg() {
+		return cpg;
 	}
 
-
-
-	public void setCor(String cor) {
-		this.cor = cor;
+	public void setCpg(String cpg) {
+		this.cpg = cpg;
 	}
-
-
 
 	public String getRg() {
 		return rg;
 	}
 
-
-
 	public void setRg(String rg) {
 		this.rg = rg;
 	}
 
-
-
-	public String getMatricula() {
+	public Integer getMatricula() {
 		return matricula;
 	}
 
-
-
-	public void setMatricula(String matricula) {
+	public void setMatricula(Integer matricula) {
 		this.matricula = matricula;
 	}
-
-
 
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + ((cor == null) ? 0 : cor.hashCode());
+		result = prime * result + ((cpg == null) ? 0 : cpg.hashCode());
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		result = prime * result + ((matricula == null) ? 0 : matricula.hashCode());
 		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
@@ -111,10 +97,10 @@ public class Aluno implements Serializable{
 		if (getClass() != obj.getClass())
 			return false;
 		Aluno other = (Aluno) obj;
-		if (cor == null) {
-			if (other.cor != null)
+		if (cpg == null) {
+			if (other.cpg != null)
 				return false;
-		} else if (!cor.equals(other.cor))
+		} else if (!cpg.equals(other.cpg))
 			return false;
 		if (id == null) {
 			if (other.id != null)
@@ -138,7 +124,6 @@ public class Aluno implements Serializable{
 			return false;
 		return true;
 	}
-	
 	
 	
 }
