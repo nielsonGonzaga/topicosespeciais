8a88d4cf3e86f2af0581f049060af7e15c5c1d8c
nielsonGonzaga <nielsondiasgonzaga123@gmail.com>
2018-10-08 21:01:11 -0300

    implementação da Cidade e Estado

--- ProjetoTopicosEspeciais/src/main/java/Fvs/edu/br/topicos/ProjetoTopicosEspeciaisApplication.java
+++ ProjetoTopicosEspeciais/src/main/java/Fvs/edu/br/topicos/ProjetoTopicosEspeciaisApplication.java
@@ -8,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import fvs.edu.br.topicos.domain.Categoria;
+import fvs.edu.br.topicos.domain.Cidade;
+import fvs.edu.br.topicos.domain.Estado;
 import fvs.edu.br.topicos.domain.Produto;
 import fvs.edu.br.topicos.repositories.CategoriaRepository;
+import fvs.edu.br.topicos.repositories.CidadeRepository;
+import fvs.edu.br.topicos.repositories.EstadoRepository;
 import fvs.edu.br.topicos.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -20,6 +24,12 @@ public class ProjetoTopicosEspeciaisApplication implements CommandLineRunner{
 	
 	@Autowired
 	ProdutoRepository prodRepository;
+	
+	@Autowired
+	CidadeRepository cidRepository;
+	
+	@Autowired
+	EstadoRepository estRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(ProjetoTopicosEspeciaisApplication.class, args);
@@ -43,5 +53,18 @@ public class ProjetoTopicosEspeciaisApplication implements CommandLineRunner{
 		
 		catRepository.saveAll(Arrays.asList(cat1, cat2));
 		prodRepository.saveAll(Arrays.asList(p1, p2, p3));
+		
+		Estado est1 = new Estado(null, "Minas Gerais");
+		Estado est2 = new Estado(null, "Ceará");
+		
+		Cidade c1 = new Cidade(null, "Uberlândia", est1);
+		Cidade c2 = new Cidade(null, "Iguatu", est2);
+		Cidade c3 = new Cidade(null, "Sobral", est2);
+		
+		est1.getCidades().addAll(Arrays.asList(c1));
+		est2.getCidades().addAll(Arrays.asList(c2, c3));
+		
+		estRepository.saveAll(Arrays.asList(est1, est2));
+		cidRepository.saveAll(Arrays.asList(c1, c2, c3));
 	}	
 }
